408e392bab22507c2a1869e97ea587f78c73c6b3
Tatenda <tatenda@Dell>
2020-07-20 15:33:32 +0200

    added passed unit test

--- S3_Backend/Lambda/test.py
+++ S3_Backend/Lambda/test.py
@@ -1,53 +1,81 @@
-# import boto3
-# import os
-# import unittest
-# from moto import mock_dynamodb2
-# from index import handler
+###Useful tutorial on how to do more check out https://fernandomc.com/posts/serverless-testing-with-moto/
 
-# def aws_setup():
-#   # Mocked AWS Credentials for moto
-#   os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
-#   os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
-#   os.environ['AWS_SECURITY_TOKEN'] = 'testing'
-#   os.environ['AWS_SESSION_TOKEN'] = 'testing'
-#   os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
+### https://stackoverflow.com/questions/48711004/how-to-mock-aws-dynamodb-service
 
-#   # Database table name into env variable
-#   os.environ['databaseName'] = 'testing'
 
-# class TestLambdaDDB(unittest.TestCase):
-#   @mock_dynamodb2
-#   def test_handler(self):
-#     # Create dynamodb boto3 object
-#     dynamodb = boto3.client('dynamodb')
-#     # Get dynamodb table name from env
-#     ddbTableName = os.environ['databaseName']
-    
-#     # Create mock table
-#     dynamodb.create_table(
-#       TableName = ddbTableName,
-#       BillingMode='PAY_PER_REQUEST',
-#       AttributeDefinitions=[
-#           {
-#               'AttributeName': 'id',
-#               'AttributeType': 'S'
-#           },
-#       ],
-#       KeySchema=[
-#           {
-#               'AttributeName': 'id',
-#               'KeyType': 'HASH'
-#           },
-#       ]
-#     )
+###importing dependencies and setup some environment variables that we’ll want set when these tests are being ru
+import boto3
+import unittest
+import os
+from moto import mock_dynamodb2
 
-#     # Print Lambda response
-#     LambdaResponse = handler(0, 0)
-#     print("Lambda response: ", LambdaResponse)
 
-#     # Run unit test against Lambda status code
-#     self.assertEqual(200, LambdaResponse['statusCode'])
 
-# if __name__ == '__main__':
-#   aws_setup()
-#   unittest.main()
+
+
+
+def aws_credentials():
+  # Mocked AWS Credentials for moto
+  os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+  os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+  os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+  os.environ['AWS_SESSION_TOKEN'] = 'testing'
+  os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
+
+
+
+class TestDynamo(unittest.TestCase):
+
+  def setUp(self):
+    pass
+
+  @mock_dynamodb2   
+  def test_handler(self):
+    # Create dynamodb boto3 object
+    dynamodb = boto3.client('dynamodb')
+    table_name = 'Dev-VisitorsCounterDynamodb-1AKIIFZ6S9KBU'
+    dynamodb = boto3.resource('dynamodb','us-east-1')
+
+    # Create mock table
+    table = dynamodb.create_table(
+            TableName=table_name,
+            KeySchema=[
+                {
+                    'AttributeName': 'key',
+                    'KeyType': 'HASH'
+                },
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'key',
+                    'AttributeType': 'S'
+                },
+
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5
+            }
+        )
+
+    item = {}
+    item['key'] = 'value'
+
+    table.put_item(Item=item)
+
+    table = dynamodb.Table(table_name)
+    response = table.get_item(
+            Key={
+                'key': 'value'
+            }
+        )
+    if 'Item' in response:
+            item = response['Item']
+
+    self.assertTrue('key' in item)
+    self.assertEqual(item['key'], 'value')
+
+
+if __name__ == '__main__':
+  aws_credentials()
+  unittest.main()
